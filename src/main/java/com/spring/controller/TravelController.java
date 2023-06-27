41c29d0e870847a9c774da4a39fdfba69a49a152
shin <wotla3009@naver.com>
2023-06-27 12:14:33 +0900

    //

--- src/main/java/com/spring/controller/TravelController.java
+++ src/main/java/com/spring/controller/TravelController.java
@@ -1,14 +1,16 @@
 package com.spring.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.service.TravelService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RequestMapping("travel")
+@RequestMapping("/travel")
 @Controller
 @Slf4j
 public class TravelController {
@@ -16,6 +18,28 @@ public class TravelController {
 	@Autowired
 	private TravelService service;
 	
+	@GetMapping("/destination")
+	public void destination() {
+		log.info("여행지 페이지 요청");
+		 
+	}
 	
+	@GetMapping("/details")
+	public void details() {
+		log.info("여행지 상세 페이지 요청");
+	}
+	
+	@GetMapping("/search")
+	public void search() {
+		log.info("검색 요청");
+	}
+	
+	@GetMapping("/tab")
+	public void tab() {
+		log.info("검색 요청");
+	}
+
 
 }
+
+
